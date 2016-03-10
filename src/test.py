ce200cfb3d20066fd3bfd0365c95b1c625e067b5
thorgerg <thorgerg@gmail.com>
2016-03-10 00:10:03 -0500

    writes tweets to twitter_data.txt

--- src/test.py
+++ src/test.py
@@ -9,9 +9,11 @@ MONGO_DB_URI = "mongodb://test_user:1234@ds053300.mlab.com:53300/emotwit2016"
 client = pymongo.MongoClient(MONGO_DB_URI)
 db = client.emotwit2016
 
+tw = open("twitter_data.txt","r+")
+
 tweets_iterator = db.twitter_collection.find()
 for tweet in tweets_iterator:
     if 'text' in tweet:
-        print (tweet['text']+ "\n")
+        tw.write(tweet['text']+ "\n")
     else:
         print ('This does not have a text entry')
