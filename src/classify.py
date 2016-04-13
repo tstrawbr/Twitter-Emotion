6e4eb34fb6ce5e37db217333a76e43e6276ae091
thorgerg <thorgerg@gmail.com>
2016-04-13 19:20:09 -0400

    runs basic naive bases classifier on a test dataset

---
+++ src/classify.py
@@ -0,0 +1,67 @@
+#import regex
+import re
+import csv
+import pprint
+import nltk.classify
+
+#start replaceTwoOrMore
+def replaceTwoOrMore(s):
+    #look for 2 or more repetitions of character
+    pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
+    return pattern.sub(r"\1\1", s)
+#end
+
+#start process_tweet
+def processTweet(tweet):
+    # process the tweets
+
+    #Convert to lower case
+    tweet = tweet.lower()
+    #Convert www.* or https?://* to URL
+    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet)
+    #Convert @username to AT_USER
+    tweet = re.sub('@[^\s]+','AT_USER',tweet)
+    #Remove additional white spaces
+    tweet = re.sub('[\s]+', ' ', tweet)
+    #Replace #word with word
+    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+    #trim
+    tweet = tweet.strip('\'"')
+    return tweet
+#end
+
+#start getStopWordList
+def getStopWordList(stopWordListFileName):
+    #read the stopwords
+    stopWords = []
+    stopWords.append('AT_USER')
+    stopWords.append('URL')
+
+    fp = open(stopWordListFileName, 'r')
+    line = fp.readline()
+    while line:
+        word = line.strip()
+        stopWords.append(word)
+        line = fp.readline()
+    fp.close()
+    return stopWords
+#end
+
+#start getfeatureVector
+def getFeatureVector(tweet, stopWords):
+    featureVector = []
+    words = tweet.split()
+    for w in words:
+        #replace two or more with two occurrences
+        w = replaceTwoOrMore(w)
+        #strip punctuation
+        w = w.strip('\'"?,.')
+        #check if it consists of only words
+        val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9]*$", w)
+        #ignore if it is a stopWord
+        if(w in stopWords or val is None):
+            continue
+        else:
+            featureVector.append(w.lower())
+    return featureVector
+#end
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import json
 import pymongo
 from flask import Flask, Response, render_template, request, redirect, url_for, send_from_directory, g, session
 from test import test
+from classify import *
 MONGO_DB_URI = "mongodb://test_user:1234@ds053300.mlab.com:53300/emotwit2016"
 client = pymongo.MongoClient(MONGO_DB_URI)
 db = client.emotwit2016
@@ -12,9 +13,53 @@ db = client.emotwit2016
 
 
 webapp = Flask(__name__)
-@webapp.route("/")
+@webapp.route("/", methods=['GET','POST'])
 def root():
-	return render_template('index.html')
+    #start extract_features
+    def extract_features(tweet):
+        tweet_words = set(tweet)
+        features = {}
+        for word in featureList:
+            features['contains(%s)' % word] = (word in tweet_words)
+        return features
+    #end
+
+    #Read the tweets one by one and process it
+    inpTweets = csv.reader(open('data/sampleTweets.csv', 'rt'), delimiter=',', quotechar='|')
+    stopWords = getStopWordList('data/stopwords.txt')
+    count = 0;
+    featureList = []
+    tweets = []
+    for row in inpTweets:
+        sentiment = row[0]
+        tweet = row[1]
+        processedTweet = processTweet(tweet)
+        featureVector = getFeatureVector(processedTweet, stopWords)
+        featureList.extend(featureVector)
+        tweets.append((featureVector, sentiment));
+    #end loop
+
+    # Remove featureList duplicates
+    featureList = list(set(featureList))
+
+    # Generate the training set
+    training_set = nltk.classify.util.apply_features(extract_features, tweets)
+
+    # Train the Naive Bayes classifier
+    NBClassifier = nltk.NaiveBayesClassifier.train(training_set)
+
+    # Test the classifier
+    if request.method == 'POST':
+        testTweet = request.form["keyword"]
+    else:
+        testTweet = 'Congrats bobbie!'
+
+    processedTestTweet = processTweet(testTweet)
+    sentiment = NBClassifier.classify(extract_features(getFeatureVector(processedTestTweet, stopWords)))
+    print ("testTweet = %s, sentiment = %s\n"% (testTweet, sentiment))
+
+    results = [testTweet, sentiment]
+    return render_template('index.html', results=results)
 
 
 
